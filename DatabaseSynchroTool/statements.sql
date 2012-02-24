d51f4d29cb101dca72809fa853d44c3bb3865547
maxime.buisson1 <maxime.buisson1@f44ff8f1-084b-338a-fdc5-81cb3f74f2ff>
2012-02-24 20:05:24 +0000

    Remove old version.

--- DatabaseSynchroTool/statements.sql
+++
@@ -1,6 +0,0 @@
-s:MS_Server: SELECT products.stockcode, products.stocklevel, products.name FROM products;
-d:RedHat_Server: UPDATE items SET  stock_level=[2], item_name="[3]" WHERE stock_code=[1];
-d:Debian_Server: UPDATE prod SET  stock_level=[2]+1, item_name="TEST" WHERE stock_code=[1];
-d:RedHat_Server: insert into test (name) values ('[3]');
-s:MS_Server: SELECT products.stockcode, products.stocklevel FROM products;
-d:Debian_Server: UPDATE prod SET  stock_level=[2]+1, item_name="TEST" WHERE stock_code=[1];
