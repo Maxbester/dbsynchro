1775277996b0ead3b57d6efecb0b487f8d05c5bf
maxime.buisson1 <maxime.buisson1@f44ff8f1-084b-338a-fdc5-81cb3f74f2ff>
2012-03-04 16:51:49 +0000

    Tag version 1.3 GUI

--- DbSyncTool/statements.sql
+++
@@ -1,4 +0,0 @@
-s:bdd1:SELECT o.num, o.date FROM orders o WHERE o.state = 'sent';
-d:bdd2:INSERT INTO oldOrders (id,date) VALUES ('[1]','[2]');
-s:bdd1:SELECT o.num, o.date FROM orders o WHERE o.state = 'received';
-d:bdd2:INSERT INTO newOrders (id) VALUES ('[1]');
